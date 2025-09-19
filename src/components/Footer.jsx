61a6b085c5e6d26c66cdee4c7807161efffb1635
Ricardo Barbosa <ricardobarbosarodrigues@gmail.com>
2025-09-19 14:53:02 -0300

    refactor: replace static links with React Router Links for terms and privacy pages

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
@@ -54,8 +55,8 @@ const Footer = () => {
               <li><a href="#">Sobre Nós</a></li>
               <li><a href="#">Contato</a></li>
               <li><a href="#">Publicidade</a></li>
-              <li><a href="#">Termos de Uso</a></li>
-              <li><a href="#">Privacidade</a></li>
+              <li><Link to="/termos">Termos de Uso</Link></li>
+              <li><Link to="/privacidade">Política de Privacidade</Link></li>
             </ul>
           </div>
         </div>
